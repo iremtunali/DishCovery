d896c7d60e11a965498d1d746d1a38b5af41cebb
İrem TUNALI <116944418+iremtunali@users.noreply.github.com>
2024-12-27 15:21:04 +0300

    favicon

--- dishcovery/src/pages/_app.js
+++ dishcovery/src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/global.css';
+import Head from "next/head";
+import "@/styles/global.css";
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>DISHCOVERY</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
